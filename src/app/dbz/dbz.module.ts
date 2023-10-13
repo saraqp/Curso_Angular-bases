8924df1664f65e61113b0f04a810b2800f48ca24
Sara <saraquesadoprado@gmail.com>
2023-10-13 10:00:44 +0200

    primer commit

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page.component';
+
+@NgModule({
+  declarations: [MainPageComponent, ListComponent, AddCharacterComponent],
+  exports: [MainPageComponent],
+  imports: [CommonModule, FormsModule],
+})
+export class DbzModule {}
